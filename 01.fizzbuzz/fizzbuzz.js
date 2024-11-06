e66490d6c24861e8f96c70893292e156d9cbc38a
sudonotfound <koxxxm@icloud.com>
2024-11-06 09:02:04 +0900

    code fix

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 for (let i = 1; i <= 20; i++) {
-  if (i % 15 == 0) {
+  if (i % 5 === 0 && i % 3 === 0) {
     console.log("Fizzbuzz");
-  } else if (i % 5 == 0) {
+  } else if (i % 5 === 0) {
     console.log("Buzz");
-  } else if (i % 3 == 0) {
+  } else if (i % 3 === 0) {
     console.log("Fizz");
   } else {
     console.log(i);
